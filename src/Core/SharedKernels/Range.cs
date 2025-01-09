b9babfe7bb695d6c65dedef71366776677f363f4
y-abbasi <y.abbasi.62@gmail.com>
2025-01-09 18:05:47 +0330

    Range value object implemented.

---
+++ src/Core/SharedKernels/Range.cs
@@ -0,0 +1,24 @@
+namespace Core.SharedKernels;
+
+public record Range<T> where T : IComparable<T>
+{
+    public Range(T Start, T End)
+    {
+        if (Start.CompareTo(End) > 0) throw new RangeIsInvalid();
+        this.Start = Start;
+        this.End = End;
+    }
+
+    public bool InRange(T value) => Start.CompareTo(value) <= 0 && value.CompareTo(End) <= 0;
+    public T Start { get; init; }
+    public T End { get; init; }
+
+    public void Deconstruct(out T Start, out T End)
+    {
+        Start = this.Start;
+        End = this.End;
+    }
+}
+public class RangeIsInvalid: Exception
+{
+}
