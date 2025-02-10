dfa0d8688ee44daf8b2871ff60878298ce364297
y-abbasi <y.abbasi.62@gmail.com>
2025-02-10 17:59:37 +0330

    implement InRange unit tests

--- src/Core.Tests/RangeTests.cs
+++ src/Core.Tests/RangeTests.cs
@@ -16,12 +16,12 @@ public class RangeTests
     {
         //arrange
         var ctor = typeof(Range<>).MakeGenericType(genericType).GetConstructors()[0];
-        start=ChangeType(start, genericType);
-        end=ChangeType(end, genericType);
-        
+        start = ChangeType(start, genericType);
+        end = ChangeType(end, genericType);
+
         //act
         var range = ctor.Invoke([start, end]);
-        
+
         //assert
         range.Should().BeEquivalentTo(new { Start = start, End = end });
     }
@@ -42,7 +42,52 @@ public class RangeTests
         //assert
         action.Should().Throw<TargetInvocationException>().And.InnerException.Should().BeOfType<RangeIsInvalid>();
     }
-    
+
+    [Theory]
+    [InlineData(10, 10, 10, typeof(int))]
+    [InlineData(10, 11, 10, typeof(long))]
+    [InlineData("2025-1-1", "2025-01-02", "2025-1-1", typeof(DateTime))]
+    [InlineData("2025-1-1", "2025-01-02", "2025-1-1", typeof(DateOnly))]
+    [InlineData("9:00:00", "9:00:01", "9:00:01", typeof(TimeOnly))]
+    public void InRange_Should_Return_True_If_Value_Be_In_Range(object start, object end, object value,
+        Type genericType)
+    {
+        //arrange
+        var ctor = typeof(Range<>).MakeGenericType(genericType).GetConstructors()[0];
+        start = ChangeType(start, genericType);
+        end = ChangeType(end, genericType);
+        value = ChangeType(value, genericType);
+        var range = ctor.Invoke([start, end]);
+
+        //act
+        var result = (bool)range.GetType().GetMethod("InRange").Invoke(range, parameters:[value]);
+
+        //assert
+        result.Should().Be(true);
+    }
+    [Theory]
+    [InlineData(10, 10, 19, typeof(int))]
+    [InlineData(10, 11, 12, typeof(long))]
+    [InlineData("2025-1-1", "2025-01-02", "2025-1-3", typeof(DateTime))]
+    [InlineData("2025-1-1", "2025-01-02", "2024-12-30", typeof(DateOnly))]
+    [InlineData("9:00:00", "9:00:01", "9:00:02", typeof(TimeOnly))]
+    public void InRange_Should_Return_False_If_Value_Not_Be_In_Range(object start, object end, object value,
+        Type genericType)
+    {
+        //arrange
+        var ctor = typeof(Range<>).MakeGenericType(genericType).GetConstructors()[0];
+        start = ChangeType(start, genericType);
+        end = ChangeType(end, genericType);
+        value = ChangeType(value, genericType);
+        var range = ctor.Invoke([start, end]);
+
+        //act
+        var result = (bool)range.GetType().GetMethod("InRange").Invoke(range, parameters:[value]);
+
+        //assert
+        result.Should().Be(false);
+    }
+
     public static object ChangeType(object value, Type targetType)
     {
         if (targetType == typeof(TimeOnly) && value is string tValue)
